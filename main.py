a740203f8c988bb3116f956075d2a898f94a8103
Oron Pariente <89079384+oronp@users.noreply.github.com>
2022-09-24 08:51:31 +0300

    auto selector + avatar creator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import random
 import cv2 as cv
 import numpy as np
@@ -10,22 +11,42 @@ BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
 BLUE = [255, 0, 0]
 GREEN = [0, 255, 0]
+NOTHING = [100, 120, 130]
+
+
+def random_selector(list_of_objects):
+    chosen = random.randint(0, len(list_of_objects) - 1)
+    return cv.imread(list_of_objects[chosen])
+
+
+def background_creator(shape, color):
+    background = np.zeros((shape[0], shape[1], 3), np.uint8)
+    changing_color(BLACK, color, background)
+    return background
 
 
 def changing_color(current_color, new_color, image):
     selected_color = np.all(image == current_color, axis=-1)  # the selected color.
     image[selected_color] = new_color
-    return image
+
+
+def add_avatar_part(current_avatar, added_object):
+    test = np.any(added_object != BLACK, axis=-1)
+    current_avatar[test] = added_object[test]
+    return current_avatar
+
+
+def draw_figure():
+    background = background_creator([300, 300], NOTHING)
+    direction_order = glob.glob('./images/*')
+    direction_order.sort(key=lambda x: int(x.split('/')[-1].split('-')[0]))
+    for single in direction_order:
+        img = random_selector(glob.glob(single + '/*'))
+        background = add_avatar_part(background, img)
+
+    cv.imshow("image", background)
+    cv.waitKey(0)
 
 
 if __name__ == '__main__':
-    first = Objects.Background()
-    black_square = np.zeros((1080, 1080, 3), np.uint8)
-    for i in range(8):
-        a = i * 135
-        cv.rectangle(black_square, (0 + a, 0 + a), (135 + a, 135 + a), WHITE, -1)
-    cv.circle(black_square, (650, 350), 75, GREEN, -1)
-    black_square = changing_color(GREEN, BLUE, black_square)
-    cv.imshow("shaked", black_square)
-    cv.waitKey(0)
-    pass
+    draw_figure()
